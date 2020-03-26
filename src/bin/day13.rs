4a28a9a51c14c817966e940d6f6f6b2b8dfd2870
Itay Bookstein <ibookstein@gmail.com>
2020-03-26 22:06:17 +0200

    AoC2019: Solve Day 13 Part 1
    Signed-off-by: Itay Bookstein <ibookstein@gmail.com>

---
+++ src/bin/day13.rs
@@ -0,0 +1,39 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+#[macro_use]
+extern crate num_derive;
+use aoc2019::aoc_input::get_input;
+use aoc2019::intcode::*;
+use num_traits::FromPrimitive;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, FromPrimitive)]
+enum Tile {
+    Empty = 0,
+    Wall = 1,
+    Block = 2,
+    Paddle = 3,
+    Ball = 4,
+}
+
+fn count_block_tiles(program: &Tape) -> usize {
+    let mut machine = IntcodeMachine::new(program.clone(), Stream::new());
+    machine
+        .run_to_completion()
+        .expect("Did not run to completion");
+
+    let mut board = HashMap::<(isize, isize), Tile>::new();
+    for (x, y, b) in machine.output.iter().tuples() {
+        let key = (*x, *y);
+        let value = Tile::from_isize(*b).expect("Bad tile number");
+        board.insert(key, value);
+    }
+
+    board.values().filter(|v| **v == Tile::Block).count()
+}
+
+fn main() {
+    let input = get_input(13);
+    let program = parse_intcode_program(&input);
+
+    println!("Block tiles count: {}", count_block_tiles(&program));
+}
