accf5ad5344cbc8edffd361e06a87edfd0bab625
Itay Bookstein <ibookstein@gmail.com>
2020-04-09 20:10:06 +0300

    AoC2019: Solve Day 19 Part 1
    Signed-off-by: Itay Bookstein <ibookstein@gmail.com>

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,5 +1,20 @@
 use aoc2019::aoc_input::get_input;
+use aoc2019::intcode::*;
 
 fn main() {
     let input = get_input(19);
+    let tape = parse_intcode_program(&input);
+
+    let mut pulled_locations = 0;
+
+    for y in 0..50 {
+        for x in 0..50 {
+            let mut machine = IntcodeMachine::new(tape.clone());
+            machine.input.borrow_mut().extend(&[x, y]);
+            machine.run().unwrap();
+            pulled_locations += machine.output.borrow_mut().pop_front().unwrap();
+        }
+    }
+
+    println!("Pulled locations: {}", pulled_locations);
 }
